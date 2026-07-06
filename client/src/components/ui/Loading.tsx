67626d6083e675e3e871b1254ebf47575ae44341
Hanbit Chang <chanbit10@gmail.com>
2026-07-06 11:36:25 +0200

    update loading

--- client/src/components/ui/Loading.tsx
+++ client/src/components/ui/Loading.tsx
@@ -2,8 +2,8 @@ import { Loader2Icon } from "lucide-react";
 
 const Loading = () => {
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-50 dark:bg-gray-900">
-      <Loader2Icon className="h-8 w-8 animate-spin text-slate-200" />
+    <div className="flex h-screen items-center justify-center primary">
+      <Loader2Icon className="h-8 w-8 animate-spin" />
     </div>
   );
 };
